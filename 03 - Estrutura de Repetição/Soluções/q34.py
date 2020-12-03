c8c868e28a4a796830ba45e8e5540afa951f2bb2
João Victor Vilela dos Santos <joaovictor.vsd@gmail.com>
2020-12-03 08:55:29 -0200

    ajustes

--- 03 - Estrutura de Repetição/Soluções/q34.py
+++
@@ -1,3 +0,0 @@
-numero = int(input('Digite: '))
-c = numero
-print(f'É primo' if numero % c == 0 and numero % 1 == 0 else 'Não é primo')
--- 03 - Estrutura de Repetição/Soluções/q35.py
+++
@@ -1,7 +0,0 @@
-num = int(input('Digite um número: '))
-print('Esses são os números primos dentro do intervalo desejado: ',end='')
-for c in range(1,num + 1):
-    if num % c == 0:
-        print(f'{c}',end='  ')
-    
-        
