ca0b82f69b23ae0a3564ed2762adf100074cf700
João Victor Vilela dos Santos <joaovictor.vsd@gmail.com>
2020-12-03 09:24:16 -0200

    Update q22.py

--- 03 - Estrutura de Repetição/Soluções/q22.py
+++ 03 - Estrutura de Repetição/Soluções/q22.py
@@ -1,11 +1,11 @@
 numero = int(input('Insira um número para teste: '))
-multiplos = 0
+divisores = 0
 
 for x in range(2,numero):
     if numero % x == 0:
-        multiplos += 1
+        divisores += 1
         print(f'{numero} é divísivel por {x}')
-if numero >= 2 and multiplos == 0:
+if numero >= 2 and divisores == 0:
     print('É PRIMO')
 else:
     print('Por isso não é primo')
