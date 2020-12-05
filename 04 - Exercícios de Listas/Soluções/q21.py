2dac9593a0b5f480d707fe1fca8fb7426a939146
João Victor Vilela dos Santos <joaovictor.vsd@gmail.com>
2020-12-05 13:06:04 -0200

    solução em python3

---
+++ 04 - Exercícios de Listas/Soluções/q21.py
@@ -0,0 +1,11 @@
+# dados fictícios dados pela questão
+lista_carros = ['fusca','gol','uno','Vectra','Peugeout']
+lista_consumo = [7,10,12.5,9,14.5]
+
+print(f'\n{"RELATÓRIO FINAL":^63}')
+print('-' * 63)
+print(f'{"CARRO":<10} {"Km/Litro":^10} {"Consumo 1000km":^20} {"Valor (em R$)":^20}')
+print('-' * 63)
+for i in range(len(lista_carros)):
+    # calculando o consumo, e o valor do consumo
+    print(f'{lista_carros[i].upper():<10} {lista_consumo[i]:^10.2f} {1000/lista_consumo[i]:^20.2f} {(1000/lista_consumo[i])*2.25:^20.2f}')
