c1e85047eb7348e7256aeead221b99ebcd99a2d6
João Victor Vilela dos Santos <joaovictor.vsd@gmail.com>
2020-12-05 13:52:35 -0200

    solução em python3

--- 04 - Exercícios de Listas/Soluções/q24.py
+++ 04 - Exercícios de Listas/Soluções/q24.py
@@ -2,9 +2,9 @@ import random
 
 dado = [0] * 6
 
-for i in range(0, 100):
+for i in range(100):
     lancamento = random.randint(0, 5)
     dado[lancamento] += 1
 
-for i in range(0, 6):
+for i in range(6):
     print(f'{i+1} - {dado[i]}')
