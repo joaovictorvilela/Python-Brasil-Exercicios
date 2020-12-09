679db406bbe2ebb6b686380bf9b0b93ece8ecf1b
João Victor Vilela dos Santos <joaovictor.vsd@gmail.com>
2020-12-09 21:45:06 -0200

    solução em python3

--- 01 - Estrutura Sequêncial/Soluções/q17.py
+++ 01 - Estrutura Sequêncial/Soluções/q17.py
@@ -1,20 +1,59 @@
-tamanho = float(input('Quantos m² devem ser pintados? '))
+area = int(input("Digite o tamanho da área: "))
 
-litros = (tamanho / 6.0) * 1.1  
-latas = int(litros / 18.0)
-galoes = int(litros / 3.6)
+#Acrescentamos os 10% de folga
+area = area*1.1
 
-if (litros % 18 != 0):
+#Agora vamos arredondar a área da seguinte maneira
+excesso = area - int(area) #aqui estamos pegando as casas decimais da area
+area = int(area) #Aqui retiramos a parte inteira da area
+
+if excesso > 0:
+    area = area + 1 #sempre devemos arredondar para cima
+
+#Vamos calcular o numero de litros necessários para pintar a casa
+litros = area//6
+if area % 6 > 0:
+    litros = litros + 1
+
+print("Litros necessários:", litros,"\n")
+
+print("1) comprar apenas latas de 18 litros")
+latas = litros//18
+if litros % 18 > 0:
+    latas = latas + 1
+
+print("Serão necessárias", latas, "latas")
+print("Obteremos", latas*18, "litros")
+print("Total: R$", latas*80)
+
+print("\n2)Comprar apenas galões de 4 litros")
+galoes = litros//4
+if litros % 4 > 0:
+    galoes = galoes + 1
+
+print("Serão necessárias", galoes, "galoes")
+print("Obteremos", galoes*4, "litros")
+print("Total: R$", galoes*25)
+
+#Vamos pensar, o preço total por litro pago nas latas é 80/18 ~ 4.44 R$/L
+#enquanto que para o gualão é 25/4 ~ 6.25 R$/L
+#portanto é sempre mais vantajoso comprar o máximo de latas possíveis
+#e o mínimo de galões, desde que o preço desses galoes não ultrapasse o preço
+#de uma lata, isto é, o numero de galoes seja menor ou igual a 3 (R$ 75)
+print("\n3)Misturar latas e galões, de forma que o preço seja o menor.")
+latas = litros//18
+galoes = 0
+litros_restantes = litros%18
+
+if litros_restantes <= 3*4:
+    #Ou seja o numero de galoes necessarios seja menor do que três
+    galoes = litros_restantes // 4
+    if litros_restantes % 4 > 0:
+        galoes += 1
+else:
     latas += 1
 
-if (litros % 3.6 != 0):
-    galoes += 1
-
-mixLatas = int(litros / 18.0)
-mixGaloes = int((litros - (mixLatas * 18.0)) / 3.6)
-if ((litros - (mixLatas * 18.0) % 3.6 != 0)):
-    mixGaloes += 1
-
-print(f'Latas: {latas} - Valor em R$: {latas*80:.2f}')
-print(f'Galões: {galoes} - Valor em R$: {galoes * 25}')
-print(f'Latas: {mixLatas} e {mixGaloes} - Valor em R$: {(mixLatas * 80) + (mixGaloes * 25)}')
+print("Serão necessárias", latas, "latas")
+print("Serão necessárias", galoes, "galoes")
+print("Obteremos", latas*18 + galoes*4, "litros")
+print("Total: R$", galoes*25 + latas*80)
