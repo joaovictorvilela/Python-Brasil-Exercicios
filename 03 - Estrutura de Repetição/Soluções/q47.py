ec924e6d87f2d3d8d7a734a483ec52ea1937d46f
João Victor Vilela dos Santos <joaovictor.vsd@gmail.com>
2020-12-10 20:57:42 -0200

    solução em python3.9.0

--- 03 - Estrutura de Repetição/Soluções/q47.py
+++ 03 - Estrutura de Repetição/Soluções/q47.py
@@ -1,23 +1,26 @@
 notas = []
 soma = 0
-
+# pedindo o nome
 nome = str(input('Nome: ')).upper()
+# pedindo as notas
 for n in range(7):
     notas.append(float(input(f'Digite a {n+1}ª nota: ')))
+    # somando as notas
     soma += notas[n]
-
+# definindo o maior e menor valor
 maior = menor = notas[0]
-
+# percorrendo a lista e verificando o maior e menor valor
 for i in notas:
     if i > maior:
         maior = i
     if i < menor:
         menor = i
-
+# subtraindo o maior e menor valor da soma
 soma -= menor 
 soma -= maior
+# calculando a média
 media = soma / 5
-
+# exibindo os resultados
 print('=' * 60)
 print(f'{"RESULTADO FINAL":^60}')
 print('=' * 60)
