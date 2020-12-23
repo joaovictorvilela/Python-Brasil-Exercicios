6cb790965c503dbd480e75d24037956c289279a1
João Victor Vilela dos Santos <74119884+joaovictorvilela@users.noreply.github.com>
2020-12-23 20:43:15 -0200

    solução em python3.9.0

--- 05 - Exercícios de Funções/Soluções/q07.py
+++ 05 - Exercícios de Funções/Soluções/q07.py
@@ -7,10 +7,14 @@ def calcularPagamento(valor = 0, dias = 0):
         return (valor + (valor * 0.03) + (valor * (dias * 0.01)))
 
 def main():
+    op = 0
     while True:
         valor = float(input('Digite o valor (em R$): '))
+        if valor != 0:
+            op += 1
         dias = int(input('Informe a quantidade de dias em atraso: '))
         if valor == 0:
             break
         print(f'Valor total: R${calcularPagamento(valor,dias):.2f}')
+        print(f'Operações realizadas: {op}')
 main()
