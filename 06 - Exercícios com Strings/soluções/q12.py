7d4e3968fa44b7762024187dce057283bde360e2
João Victor Vilela dos Santos <74119884+joaovictorvilela@users.noreply.github.com>
2020-12-27 21:16:10 -0200

    Update q12.py

--- 06 - Exercícios com Strings/soluções/q12.py
+++ 06 - Exercícios com Strings/soluções/q12.py
@@ -1,6 +1,6 @@
 def valida_numero(numero):
-    com_f = '3' + numero[:4] + '-' + numero[4:]
-    sem_f = '3' + numero[:4] + numero[4:]
+    com_f = '3' + numero[:3] + '-' + numero[3:]
+    sem_f = '3' + numero[:3] + numero[3:]
     if len(numero) == 7:
         print('Telefone possui 7 dígitos. Vou acrescentar o digito três na frente.')
         return f'Número com formatação: {com_f}\nNúmero sem formatação: {sem_f}'
